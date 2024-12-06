d55e9ad748026a8299260a85513b441dba0874e2
bruthalles <thallesgmkr@gmail.com>
2024-12-05 22:45:07 -0300

    testando objetos locais separados

---
+++ local-posts.cjs
@@ -0,0 +1,22 @@
+
+
+
+const posts = [
+    {
+      id:1,
+      descricao: "gato voador",
+      imagem: "https://placecats.com/300/300"
+    },
+    {
+      id:2,
+      descricao: "gato ninja",
+      imagem: "https://placecats.com/300/300"
+    },
+    {
+      id:3,
+      descricao: "gato presidente",
+      imagem: "https://placecats.com/300/300"
+    }
+  ];
+
+  module.exports = posts;
